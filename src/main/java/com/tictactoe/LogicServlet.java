6c19d7c64b65eee4f46dc1636319ef10d16a9c51
RuslanVrubleuski <6628762@mail.ru>
2024-05-09 12:39:02 +0300

    There is written a code mapping crosses in the table

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -3,18 +3,46 @@ package com.tictactoe;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LogicServlet", value = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        int index = getSelectedIndex(request);
-        response.sendRedirect("/index.jsp");
-        System.out.println("R");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // Получаем текущую сессию
+        HttpSession currentSession = req.getSession();
+
+        // Получаем объект игрового поля из сессии
+        Field field = extractField(currentSession);
+
+        // получаем индекс ячейки, по которой произошел клик
+        int index = getSelectedIndex(req);
+
+        // ставим крестик в ячейке, по которой кликнул пользователь
+        field.getField().put(index, Sign.CROSS);
+
+        // Считаем список значков
+        List<Sign> data = field.getFieldData();
+
+        // Обновляем объект поля и список значков в сессии
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
+
+        resp.sendRedirect("/index.jsp");
     }
+
     private int getSelectedIndex(HttpServletRequest request) {
         String click = request.getParameter("click");
         boolean isNumeric = click.chars().allMatch(Character::isDigit);
         return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) fieldAttribute;
     }
 }
